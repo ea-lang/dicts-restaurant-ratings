00d55a7a6dbd9af12812c38f8415dbcbf27d9e90
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 17:50:40 -0800

    Added a menu to prompt user for action and process user selection

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -2,7 +2,10 @@ import sys
 
 
 def read_restaurant_file(file_name):
-    """Parses a file of restaurant names and ratings (format restaurant:rating) and adds them to a dict"""
+    """Parse a file of restaurant names/ratings and return a dict of them
+
+    (format of the file's lines is restaurant:rating)
+    """
 
     ratings = {}
     restaurant_file = open(file_name)
@@ -16,24 +19,45 @@ def read_restaurant_file(file_name):
     return ratings
 
 
-def add_rating(ratings):
+def add_rating(ratings_to_add):
+    """Prompt user for a new restaurant and rating, return updated dict"""
     new_restaurant = raw_input("Enter a restaurant name:")
     new_rating = raw_input("Enter the rating:")
 
-    ratings[new_restaurant] = new_rating
+    ratings_to_add[new_restaurant] = new_rating
 
-    return ratings
+    return ratings_to_add
 
 
-def print_alpha_restaurant_ratings(rating_dict):
-    """Prints restaurant names and ratings from a dict in alphabetical order"""
+def print_alpha_restaurant_ratings(ratings_to_sort):
+    """Print restaurant names and ratings from a dict in alphabetical order"""
 
-    sorted_restaurants = sorted(rating_dict.items())
+    sorted_restaurants = sorted(ratings_to_sort.items())
     for restaurant, rating in sorted_restaurants:
         print "%s is rated at %s." % (restaurant, rating)
 
 
+def choose_action(file_name):
+    """Display menu, prompt user for action and process user selection"""
+
+    restaurant_ratings = read_restaurant_file(file_name)
+
+    while True:
+        print """Menu options:
+                1. Display restaurant ratings
+                2. Add a new rating
+                3. Quit"""
+        user_action = raw_input(">>")
+
+        if user_action == '1':
+            print_alpha_restaurant_ratings(restaurant_ratings)
+        elif user_action == '2':
+            restaurant_ratings = add_rating(restaurant_ratings)
+        elif user_action == '3':
+            break
+        else:
+            print "Not a valid option. Please try again."
+
+
 file_name = sys.argv[1]
-restaurant_ratings = read_restaurant_file(file_name)
-add_rating(restaurant_ratings)
-print_alpha_restaurant_ratings(restaurant_ratings)
+choose_action(file_name)
