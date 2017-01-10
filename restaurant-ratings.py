8d1dd540eed49761e2ea5821c7cc917384b7fa80
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 16:32:28 -0800

    Added ability for user to add a restaurant and rating before printing the dictionary

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,32 +1,39 @@
-# your code goes here
 import sys
 
 
-restaurant_ratings_dict = {}
-
-
 def read_restaurant_file(file_name):
-    """Parses a file of restaurant names and scores and adds them to a dict"""
+    """Parses a file of restaurant names and ratings (format restaurant:rating) and adds them to a dict"""
+
+    ratings = {}
     restaurant_file = open(file_name)
+
     for line in restaurant_file:
-        line = line.rstrip()
-        restaurant_pairs = line.split(':')
+        restaurant_tokens = line.rstrip().split(':')
+        restaurant, rating = restaurant_tokens
 
-        restaurant = restaurant_pairs[0]
-        rating = restaurant_pairs[1]
+        ratings[restaurant] = rating
 
-        restaurant_ratings_dict[restaurant] = rating
+    return ratings
 
 
-def print_alpha_restaurant_ratings(dict):
+def add_rating(ratings):
+    new_restaurant = raw_input("Enter a restaurant name:")
+    new_rating = raw_input("Enter the rating:")
+
+    ratings[new_restaurant] = new_rating
+
+    return ratings
+
+
+def print_alpha_restaurant_ratings(rating_dict):
     """Prints restaurant names and ratings from a dict in alphabetical order"""
-    sorted_restaurants = sorted(dict.items())
+
+    sorted_restaurants = sorted(rating_dict.items())
     for restaurant, rating in sorted_restaurants:
         print "%s is rated at %s." % (restaurant, rating)
 
 
 file_name = sys.argv[1]
-
-read_restaurant_file(file_name)
-
-print_alpha_restaurant_ratings(restaurant_ratings_dict)
+restaurant_ratings = read_restaurant_file(file_name)
+add_rating(restaurant_ratings)
+print_alpha_restaurant_ratings(restaurant_ratings)
